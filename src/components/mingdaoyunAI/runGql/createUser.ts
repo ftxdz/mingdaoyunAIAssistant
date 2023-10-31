1cb342b68a68fff8e3b2d066967b95e63af98888
huangweixi <huangweixi@functorz.com>
2023-10-31 13:16:11 +0800

    添加欢迎语

--- src/components/mingdaoyunAI/runGql/createUser.ts
+++ src/components/mingdaoyunAI/runGql/createUser.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { runGql } from "./runGql";
-const greetingMessage = "你好呀";
+const greetingMessage = "Hello 同学～  欢迎了解Zion，很高兴我们能在一年一度的伙伴大会相见，如果你对Zion无代码开发工具以及我们和明道云的故事感兴趣，可以跟我聊聊哦";
 const gql: string = `mutation insertUser($uuid: String!, $greetingMessage: String) {
     insert_user_one(
       object: {
